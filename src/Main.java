f6c8435145ea9f08f53435340fd7af7cec9e95de
Santiago Rafael Escobar <raescobar@uy000gvy7lgd4vm.ml.com>
2023-09-07 11:48:29 -0300

    I add the class Fox

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@
 public class Main {
     public static void main(String[] args) {
         Animal a1 = new Dog();
+        Animal a2 = new Fox();
         a1.makeSound();
+        a2.makeSound();
 
     }
 
