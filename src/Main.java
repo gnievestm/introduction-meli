cfc895dc11bf253d4e5793af8edad8733900a51c
leosilva12 <leonardo.silva@mercadolibre.com>
2023-09-07 11:22:23 -0300

    Leo lo logro :D

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
         Animal a1 = new Dog();
         a1.makeSound();
+        Animal a2 = new Cat();
+        a2.makeSound();
 
     }
 
